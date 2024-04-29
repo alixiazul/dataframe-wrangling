dad05f63c16dd1896a06b22258262eaebe2d933e
Joe Mulvey <joe.mulvey@northcoders.com>
2024-04-29 20:01:29 +0100

    Add json data and skeleton files

---
+++ src/json_data.py
@@ -0,0 +1,8 @@
+def create_df(filename):
+    pass
+
+def increase_price(df):
+    pass
+
+def get_best_value(df):
+    pass
---
+++ tests/test_json_data.py
@@ -0,0 +1,2 @@
+from src.json_data import create_df, increase_price, get_best_value
+
