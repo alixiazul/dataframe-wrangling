32dff9c07d772124cba39215c86d4bb15a77d4f1
filipeorfao <filipeaorfao@gmail.com>
2024-05-07 12:33:45 +0100

    start testing increase_price

--- src/json_data.py
+++ src/json_data.py
@@ -5,8 +5,8 @@ def create_df(filename):
     return pandas.read_json(filename)
 
 
-def increase_price(df):
-    pass
+def increase_price(df, percent):
+    return df.mul({"price", percent / 100 + 1})
 
 
 def get_best_value(df):
--- tests/test_json_data.py
+++ tests/test_json_data.py
@@ -1,10 +1,25 @@
 from src.json_data import create_df, increase_price, get_best_value
 
-# import pandas as pd
+import pandas as pd
 
 
 class TestCreateDf:
     def test_returns_dataframe(self):
-        # test_df = pandas.DataFrame()
-        # assert type(test_df) == type(create_df("data/doughnuts.json"))
-        assert False == type(create_df("data/doughnuts.json"))
+        test_df = pd.DataFrame()
+        result = create_df("data/doughnuts.json")
+        assert type(test_df) == type(result)
+
+
+class TestIncreasePrice:
+    # def test_does_not_mutate(self):
+    #     assert False
+
+    def test_(self):
+        df = create_df("data/doughnuts.json")
+        df2 = increase_price(df, 5).price
+        assert df.mul("price", 1.05) == df2
+
+
+class TestGetBestValue:
+    def test_(self):
+        assert False
